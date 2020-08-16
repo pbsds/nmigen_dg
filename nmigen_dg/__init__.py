213c158748315668c48b972afc7ea251992a2196
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-08-16 22:47:20 +0200

    Add tracer.skip_frame, use it to remove all get_src_loc errors
    tracer.skip_frame overrides nmigen.tracer.get_src_loc with an offset

--- nmigen_dg/__init__.py
+++ nmigen_dg/__init__.py
@@ -1,4 +1,13 @@
-import dg # dogelang import hook
+# dogelang import hook
+import dg
+
+# hook into the nmigen 'get_src_loc' function
+from . import tracer
+
+# import the DSL
 from .dsl import *
+
+# cleanup the namespace
+del dg, tracer, dsl
+
 __version__ = "0.3.0"
-del dg
